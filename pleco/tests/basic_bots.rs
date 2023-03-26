55c875c5aba1272dc24b22bcadb855b2b40d836e
tjf801 <fuscofamily0@gmail.com>
2023-03-26 19:53:24 -0400

    en passant is now mandatory

--- pleco/tests/basic_bots.rs
+++ pleco/tests/basic_bots.rs
@@ -15,3 +15,24 @@ fn test_all_bot() {
         JamboreeSearcher::best_move(board.shallow_clone(), 4);
     }
 }
+
+#[test]
+fn test_bot_en_passant() {
+    // taking with rook is fine (but brick on pipi), taking en passant leads to mate in 1
+    let board = Board::from_fen("2r3k1/5ppp/8/1pP5/8/8/1R3PPP/6K1 w - b6 0 2").unwrap();
+    
+    let _move = AlphaBetaSearcher::best_move(board.shallow_clone(), 4);
+    assert_eq!(_move.flag(), pleco::core::piece_move::BitMove::FLAG_EP);
+    
+    let _move = JamboreeSearcher::best_move(board.shallow_clone(), 4);
+    assert_eq!(_move.flag(), pleco::core::piece_move::BitMove::FLAG_EP);
+    
+    let _move = MiniMaxSearcher::best_move(board.shallow_clone(), 4);
+    assert_eq!(_move.flag(), pleco::core::piece_move::BitMove::FLAG_EP);
+    
+    let _move = ParallelMiniMaxSearcher::best_move(board.shallow_clone(), 4);
+    assert_eq!(_move.flag(), pleco::core::piece_move::BitMove::FLAG_EP);
+    
+    let _move = RandomBot::best_move(board.shallow_clone(), 4);
+    assert_eq!(_move.flag(), pleco::core::piece_move::BitMove::FLAG_EP);
+}
